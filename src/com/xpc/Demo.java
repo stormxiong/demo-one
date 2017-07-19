5712551cf20b32eab18da27402d6ce06c2562267
stormxiong <373250051@qq.com>
2017-07-19 22:52:07 +0800

    Initial commit

---
+++ src/com/xpc/Demo.java
@@ -0,0 +1,10 @@
+package com.xpc;
+
+/**
+ * Created by xiongpc on 2017/7/19.
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
